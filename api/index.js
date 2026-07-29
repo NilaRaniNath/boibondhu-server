882e023e64be2821edecd57fc83ea4320bd87819
SportNest <student@sportnest.com>
2026-07-29 10:50:49 +0600

    Add try-catch to minimal api function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true, url: req.url, method: req.method }));
+module.exports = async (req, res) => {
+  try {
+    console.log('Function invoked:', req.method, req.url);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ ok: true, url: req.url, method: req.method }));
+  } catch (err) {
+    console.error('Function error:', err);
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: err.message }));
+  }
 };
