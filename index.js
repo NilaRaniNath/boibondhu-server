3b6009d5f4ef8c69e0c2a297c411753ee62bf8ae
SportNest <student@sportnest.com>
2026-07-29 10:36:10 +0600

    Use @vercel/node builder with dist/index entry (all deps traced)

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-const express = require('express');
-const app = express();
-const results = [];
-
-function testRequire(name) {
-  try {
-    require(name);
-    results.push({ name, ok: true });
-  } catch (e) {
-    results.push({ name, ok: false, error: e.message });
-  }
-}
-
-testRequire('mongodb');
-testRequire('zod');
-testRequire('bcryptjs');
-testRequire('jsonwebtoken');
-testRequire('cookie-parser');
-testRequire('cors');
-testRequire('dotenv');
-
-app.get('/', (req, res) => res.json({ ok: true, results }));
+const app = require('./dist/index').default;
 module.exports = app;
