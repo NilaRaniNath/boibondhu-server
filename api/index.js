54fa12a14600a0a72b350386fe0024897703e909
SportNest <student@sportnest.com>
2026-07-29 10:54:31 +0600

    ULTRA minimal: just res.end('ok')

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-module.exports = async (req, res) => {
-  try {
-    console.log('Function invoked:', req.method, req.url);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ ok: true, url: req.url, method: req.method }));
-  } catch (err) {
-    console.error('Function error:', err);
-    res.statusCode = 500;
-    res.end(JSON.stringify({ error: err.message }));
-  }
+module.exports = (req, res) => {
+  res.end('ok');
 };
